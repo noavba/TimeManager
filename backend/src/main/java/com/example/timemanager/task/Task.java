e1dc1f2549fa8f9b5144ed385409a35379263a62
noavba <102984679+noavba@users.noreply.github.com>
2024-06-20 15:54:12 -0300

    pushed small changes to pick up on laptop

--- backend/src/main/java/com/example/timemanager/task/Task.java
+++ backend/src/main/java/com/example/timemanager/task/Task.java
@@ -8,6 +8,17 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+    private String description;
+    private String date;
+    private String timestamp;
+    private String title;
+
+    public Task(String date, String description, String title) {
+        this.date = date;
+        this.description = description;
+        this.title = title;
+    }
+
 
     public Long getId() {
         return id;
